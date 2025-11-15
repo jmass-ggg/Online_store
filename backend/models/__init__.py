c9b43f4211230c5706a179031d6a55b25da8018b
jmass-ggg <jamesgurung690@gmail.com>
2025-11-15 22:19:01 +0545

    customer

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -3,3 +3,4 @@ from .product import Product
 from .order import Order
 from .order_iteam import OrderItem
 from .role import Roles
+from .admin import Admin
---
+++ backend/models/admin.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from backend.database import Base
+from sqlalchemy import Integer,String,Float,ForeignKey,Column,DateTime, func
+from datetime import datetime
+from sqlalchemy.orm import relationship,Mapped,mapped_column
+
+class Admin(Base):
+    
+    __tablename__ = "admin"
+    
+    id : Mapped[int]=mapped_column(Integer, primary_key=True, index=True)
+    username : Mapped[str]=mapped_column(String, unique=True, nullable=False)
+    email : Mapped[str]=mapped_column(String, unique=True, index=True, nullable=False)
+    hashed_password : Mapped[str]=mapped_column(String, nullable=False)
+    created_at : Mapped[DateTime]=mapped_column(DateTime, default=datetime.utcnow)
+    updated_at : Mapped[DateTime]=mapped_column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    role_name : Mapped[int] = mapped_column(ForeignKey("roles.role_name"), default="Admin")
+    
+    role : Mapped["Roles"]=relationship("Roles",back_populates="admins")
+    
+    def __repr__(self)->str:
+        return f"<Admin(username={self.username},email={self.email})>"
