0de1131cd37bbd482aa947bb8f3d14d61b9e88ea
jmass-ggg <jamesgurung690@gmail.com>
2025-11-26 22:04:01 +0545

    debug

--- backend/api/v1/product.py
+++ backend/api/v1/product.py
@@ -4,7 +4,7 @@ from typing import List
 
 from backend.database import get_db
 from backend.schemas.product import Product_create, Product_read, Product_update
-from backend.models.customer import Customer
+from backend.models.seller import Seller
 from backend.utils.jwt import get_seller_from_refresh
 from backend.service.product_service import (
     add_product_by_seller,
@@ -28,8 +28,7 @@ def product_add(
     description: str = Form(...),
     image: UploadFile = File(...),
     db: Session = Depends(get_db),
-    current_user: Customer = Depends(require_permission("add_product"))
-):
+    current_user: Seller = get_seller_from_refresh):
     return add_product_by_seller(product_name,product_category,stock,price,description,image,db,current_user,UPLOAD_FOLDER)
 
 @router.patch("/{product_id}", response_model=Product_read)
@@ -37,27 +36,31 @@ def product_edit(
     product_id: int,
     product_update: Product_update,
     db: Session = Depends(get_db),
-    current_user: Customer = Depends(get)
+    current_user: Seller = Depends(get_seller_from_refresh)
 ):
     
     return edit_product_by_seller(db,product_id,product_update,current_user)
 
 @router.delete("/{product_id}/admin",status_code=status.HTTP_200_OK)
 def delete_product(product_id:int,db:Session=Depends(get_db),
-                current_user:Customer=Depends(require_permission("delete_any_product"))):
+                current_user:Seller=Depends(get_seller_from_refresh)
+):
     return delete_product_by_admin(db,product_id,current_user)
 
 @router.delete("/{product_id}/seller",status_code=status.HTTP_200_OK)
 def delete_product(product_id:int,db:Session=Depends(get_db),
-                current_user:Customer=Depends(require_permission("delete_own_product"))):
+                current_user:Seller=Depends(get_seller_from_refresh)
+):
     return delete_product_by_seller(db,product_id)
 
 @router.get("/{product_id}",response_model=Product_read)
 def get_product(product_id:int,db:Session=Depends(get_db),
-                current_user:Customer=Depends(require_permission("view_own_orders"))):
+                current_user:Seller=Depends(get_seller_from_refresh)
+):
     return veiw_product(db,product_id,current_user)
 
 @router.get("/",response_model=List[Product_read])
 def get_all_product(db:Session=Depends(get_db)
-                    ,current_user:Customer=Depends(require_permission("view_own_orders"))):
+                    ,current_user:Seller=Depends(get_seller_from_refresh)
+):
     return view_all_product(db)
